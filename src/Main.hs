e87aa5ff8ff5ca28cb4c2799e37af50ba5156413
Nishant Chhillar <nchhillar2004@gmail.com>
2026-02-15 11:41:48 +0530

    initial commit

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main (main) where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found match"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    (expr:_) <- getArgs
+    putStrLn (readExpr expr)
